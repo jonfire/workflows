a0bf184b31da8adbaff681b5634993038f2bbd14
Jon Warnes <jonwarnes@yahoo.co.uk>
2016-03-29 18:57:35 +0100

    set up gulp file to shift html to production

--- gulpfile.js
+++ gulpfile.js
@@ -72,7 +72,7 @@ gulp.task('watch', function() {
 	gulp.watch(coffeeSources, ['coffee']);
 	gulp.watch(jsSources, ['js']);
 	gulp.watch('components/sass/*.scss', ['compass']);
-	gulp.watch(htmlSources, ['html']);
+	gulp.watch('builds/development/*.html', ['html']);
 	gulp.watch(jsonSources, ['json']);
 });
 
@@ -84,8 +84,9 @@ gulp.task('connect', function() {
 });
 
 gulp.task('html', function() {
-	gulp.src(htmlSources)
+	gulp.src('builds/development/*.html')
 	.pipe(connect.reload())
+	.pipe(gulpif(env === 'production', gulp.dest(outputDir)))
 })
 
 gulp.task('json', function() {
